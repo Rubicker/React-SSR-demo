28af797f962607ecc89a3018a03e5537fea7d76f
Rubicker <newsl00001@gmail.com>
2017-12-26 14:51:20 +0800

    Add styled-component for SSR

--- src/server.js
+++ src/server.js
@@ -3,17 +3,24 @@ import React from 'react'
 import { renderToString } from 'react-dom/server'
 import App from './client/App'
 import Html from './client/Html'
+// this is important
+import { ServerStyleSheet } from 'styled-components'
 
 const port = 3000
 const server = express()
 
 server.get('/', (req, res) => {
-  const body = renderToString(<App />)
+  const sheet = new ServerStyleSheet()  // create our stylesheet
+
+  const body = renderToString(sheet.collectStyles(<App />))  // collect styles
+  const styles = sheet.getStyleTags()  // get all the tags from the sheet
   const title = 'SSR with Styled Components'
+
 
   res.send(
     Html({
       body,
+      styles,  // pass the styles to our template
       title
     })
   )
